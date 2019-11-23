21fd7398bc12794aab16718e3ccfa88b80e542fe
Cory <cjl9703@yahoo.com>
2019-11-23 13:09:31 -0800

    (init) project initialized with Core Data

---
+++ app/OutdoorIO/OutdoorIO/CoreDataModel/Pin+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Pin+CoreDataProperties.swift
+//  OutdoorIO
+//
+//  Created by Cory Lewis on 11/23/19.
+//  Copyright © 2019 CR. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Pin {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Pin> {
+        return NSFetchRequest<Pin>(entityName: "Pin")
+    }
+
+    @NSManaged public var longitude: Float
+    @NSManaged public var latitude: Float
+    @NSManaged public var img: Data?
+    @NSManaged public var thumb: Data?
+    @NSManaged public var msg: String?
+    @NSManaged public var timestamp: Date?
+    @NSManaged public var trip: Trip?
+
+}
---
+++ app/OutdoorIO/OutdoorIO/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  OutdoorIO
+//
+//  Created by Cory Lewis on 11/23/19.
+//  Copyright © 2019 CR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello World!")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
