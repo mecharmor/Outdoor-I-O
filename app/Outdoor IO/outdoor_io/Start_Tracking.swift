1f0579aea4a399450c72d2782a373386f48dc139
bittersweetsalt <38972085+bittersweetsalt@users.noreply.github.com>
2019-12-06 13:54:47 -0800

    Implement(ing) Camera
    Current Camera would require IPhone to test out.
    Work on logic of saving later.
    [TODO]
    Passing coordinates and image to save

--- app/Outdoor IO/outdoor_io/Start_Tracking.swift
+++ app/Outdoor IO/outdoor_io/Start_Tracking.swift
@@ -73,7 +73,7 @@ class Start_Tracking: UIViewController {
             locationManager.delegate = self
             locationManager.startUpdatingLocation()
             
-            Model.I.newTrip(name: "Trip 1") // Sample Interactions with the database
+            //Model.I.newTrip(name: "Trip 1") // Sample Interactions with the database
             // Model.I.newPin(img: <#T##UIImage#>, msg: "hello!!!")
             // Upon delegate actions we need to take picture and invoke
             // Model.I.newPin(img: <#T##UIImage#>, msg: <#T##String#>)
--- app/Outdoor IO/outdoor_io/ViewController.swift
+++ app/Outdoor IO/outdoor_io/ViewController.swift
@@ -15,24 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var history: UIButton!
     @IBOutlet weak var settings: UIButton!
     
-    /*1. --- button settings*/
-    func button_settings() {
-        // button view definitions
-        // .clipsToBounds are meant for images
-        start_tracking.layer.cornerRadius = 8.0
-        start_tracking.clipsToBounds = true
-        history.layer.cornerRadius = 8.0
-        history.clipsToBounds = true
-        settings.layer.cornerRadius = 8.0
-        settings.clipsToBounds = true
-    }
     
-    /*2. --- backdrop settings*/
-    func screen_backdrop() {
-        // Set BackGround color for screens
-        
-        view.backgroundColor = UIColor.init(red: 46/255, green: 64/255, blue: 56/255, alpha: 0.8)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,4 +34,27 @@ class ViewController: UIViewController {
     }
     
     @IBAction func unwind_to_main(_ sender: UIStoryboardSegue){}
+}
+
+extension ViewController{
+    
+   /*1. --- button settings*/
+    func button_settings() {
+        // button view definitions
+        // .clipsToBounds are meant for images
+        start_tracking.layer.cornerRadius = 8.0
+        start_tracking.clipsToBounds = true
+        history.layer.cornerRadius = 8.0
+        history.clipsToBounds = true
+        settings.layer.cornerRadius = 8.0
+        settings.clipsToBounds = true
+    }
+    
+    /*2. --- backdrop settings*/
+    func screen_backdrop() {
+        // Set BackGround color for screens
+        
+        view.backgroundColor = UIColor.init(red: 46/255, green: 64/255, blue: 56/255, alpha: 0.8)
+    }
+    
 }
