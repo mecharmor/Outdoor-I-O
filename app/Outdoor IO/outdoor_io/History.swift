2ae9a116746fd985830b209c3d9c7420a7f9828a
bittersweetsalt <38972085+bittersweetsalt@users.noreply.github.com>
2019-12-08 16:58:55 -0800

    Starting with tablecell images
    [todo]
    figure out actual placement and outlet issue.
    - There will be a need to create a data object by joining Trips and Pins together.
    - example: https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/CreateATableView.html

--- app/Outdoor IO/outdoor_io/History.swift
+++ app/Outdoor IO/outdoor_io/History.swift
@@ -8,22 +8,51 @@
 
 import UIKit
 
-class History: UIViewController {
+class History: UITableViewController {
 
-    @IBOutlet weak var Back: UIButton!
+    // let location_list = ["Socal", "Sacremento", "Montreal", "Monterey"]
     
+    // Get all Trip data from Model
+    // * Encased in brackets as Model.I.[func] returns an object
+    var trip_name = [Model.I.getAllTrips]
+    // Get all Pin data from Model
+    // * getTripPins also returns an image within the return
+    var pin_location = [Model.I.getTripPins]
+        
+    
+    
+    @IBOutlet weak var location: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Screen + Button Setup
         screen_backdrop()
         button_settings()
+        
         // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return location_list.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "history_of_pins", for: indexPath)
+        
+        cell.textLabel?.text = location_list[indexPath.row]
+        
+        return cell
     }
     
 }
@@ -33,9 +62,9 @@ extension History{
     // --- button settings --- //
     func button_settings() {
         // button view definitions
-        Back.backgroundColor = UIColor.init(red: 46/255, green: 89/255, blue: 70/255, alpha: 1)
-        Back.layer.cornerRadius = 5.0
-        
+//        _back.backgroundColor = UIColor.init(red: 46/255, green: 89/255, blue: 70/255, alpha: 1)
+//        _back.layer.cornerRadius = 5.0
+//
     }
     
     //--- backdrop settings --- //
